0fa37f87409d2661a3f4e8a03ab3b325f4ba8140
Gavan Lamb <gavan.jon.lamb@gmail.com>
2017-06-10 11:11:59 +1000

    Updated google cloud script to run commands sequentially

--- 1-gcloud-install.sh
+++ 1-gcloud-install.sh
@@ -2,12 +2,12 @@
 echo -e "\e[42m\e[39m                                           \e[0m\e[0m";
 echo -e "\e[42m\e[39m               GCLOUD INSTALL              \e[0m\e[0m";
 echo -e "\e[42m\e[39m                                           \e[0m\e[0m";
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)" ;
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list ;
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - ;
 sudo apt-get update && sudo apt-get install -y google-cloud-sdk ;
 
 echo -e "\e[42m\e[39m                                           \e[0m\e[0m";
 echo -e "\e[42m\e[39m          PLEASE INITIALIZE GCLOUD         \e[0m\e[0m";
 echo -e "\e[42m\e[39m                                           \e[0m\e[0m";
-gcloud init 
+gcloud init ;
